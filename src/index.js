e3a39ff6e12ce3554f044006b006c67597837438
Fernando Yepes <yepesjuanfer7532@gmail.com>
2022-10-17 18:54:32 -0500

    correccion error en menu mobile y tarjetas de skills agregadas

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
+//Helpers
+
+function closeMobileMenu (){
+
+    burgerMenu.classList.remove('box-transition');
+    stick1.classList.toggle('animation1');
+    stick3.classList.toggle('animation2');
+    stick2.classList.toggle('inactive');
+    burgerMenu.classList.add('reverse');
+    mobileMenu.classList.toggle('inactive');
+    blur.classList.toggle('inactive');
+    body.classList.remove('no-scroll')
+}
+
+//Events
 burgerMenu.addEventListener('click', () => {
 
     if(!burgerMenu.classList.contains('box-transition')){
@@ -6,26 +21,31 @@ burgerMenu.addEventListener('click', () => {
         stick1.classList.toggle('animation1');
         stick3.classList.toggle('animation2');
         stick2.classList.toggle('inactive');
-        burgerMenu.classList.toggle('box-transition');
+        burgerMenu.classList.add('box-transition');
         mobileMenu.classList.toggle('inactive');
         blur.classList.toggle('inactive');
         body.classList.add('no-scroll')
 
+
     }else{
 
-        burgerMenu.classList.remove('box-transition');
-        stick1.classList.toggle('animation1');
-        stick3.classList.toggle('animation2');
-        stick2.classList.toggle('inactive');
-        burgerMenu.classList.add('reverse');
-        mobileMenu.classList.toggle('inactive');
-        blur.classList.toggle('inactive');
-        body.classList.remove('no-scroll')
+        closeMobileMenu();
 
     }
 
     
 });
+
+navbarLink.forEach(item =>{
+        
+    item.addEventListener('click', ()=>{
+
+        if (body.classList.contains('no-scroll')) {
+
+            closeMobileMenu();
+        }
+    })
+})
 
 sendBtn.addEventListener('click', (e)=>{
 
@@ -67,7 +87,7 @@ sendBtn.addEventListener('click', (e)=>{
             setTimeout(()=>{
     
     
-            window.location.href=`mailto:yepesjuanfer7532@gmail.com?subject=Envío%20desde%20formulario&body=Nombre%3A%20${name}%0AEmail%3A%20${email}%0AMensaje%3A%20${message}`;
+            window.location.href=`mailto:fernandoym75@gmail.com?subject=Envío%20desde%20formulario&body=Nombre%3A%20${name}%0AEmail%3A%20${email}%0AMensaje%3A%20${message}`;
             
             sendBtn.classList.toggle('plane');
     
@@ -109,9 +129,198 @@ moreContentBtn.addEventListener('click', ()=>{
         })
         
     }
-
-
-    
 })
 
 upBtn.addEventListener('click', () => window.scrollTo({top:0}));
+
+
+let cards = [];
+cards.push({
+
+    'iconClass1': 'fa-square-github',
+    'iconClass2': 'fa-git-alt',
+    'skillName': 'Git y GitHub',
+    'text': 'Tengo conocimientos en Git y GitHub para el desarrollo en equipo manejando este sistema de control de versiones, puedes ir a mi perfil en LinkedIn para ver mis certificados',
+    'id': 1,
+    'categoryBlock': 'first-tech-block',
+})
+
+cards.push({
+
+    'iconClass1': 'fa-html5',
+    'iconClass2': '.',
+    'skillName': 'HTML',
+    'text': '',
+    'id': 2,
+    'categoryBlock': 'second-tech-block',
+
+})
+
+cards.push({
+    'iconClass1': 'fa-css3-alt',
+    'iconClass2': '.',
+    'skillName': 'CSS',
+    'text': '',
+    'id': 3,
+    'categoryBlock': 'third-tech-block',
+
+})
+
+cards.push({
+    'iconClass1': 'fa-square-js',
+    'iconClass2': '.',
+    'skillName': 'JavaScript',
+    'text': '',
+    'id': 4,
+    'categoryBlock': 'fourth-tech-block',
+})
+
+cards.push({
+
+    'iconClass1': 'fa-square-js',
+    'iconClass2': '.',
+    'skillName': 'TypeScript',
+    'text': '',
+    'id': 5,
+    'categoryBlock': 'fifth-tech-block',
+})
+
+function cardSkillRender(skillsArray){
+
+    skillsArray.forEach(skill => {
+        
+        const card = document.createElement('div');
+        card.classList.add(`${skill.categoryBlock}`, 'skills-block');
+
+        if (skill.categoryBlock != 'fifth-tech-block') {
+            
+        
+            const icon1 = document.createElement('i');
+            icon1.classList.add('fa-brands', `${skill.iconClass1}`, 'skills-icons');
+        
+            const techName = document.createElement('p');
+            techName.textContent = `${skill.skillName}`;
+        
+            const icon2 = document.createElement('i');
+            icon2.classList.add('fa-brands', `${skill.iconClass2}`, 'skills-icons');
+
+            card.addEventListener('click', ()=>{
+    
+                renderingDetailSkill(skill)
+                window.scrollTo({top:250})
+                cardContainer.classList.remove('inactive');
+                body.classList.add('no-scroll');
+            })
+        
+            card.append(icon1, techName, icon2)
+            skillTechContianer.append(card)
+
+        }else{
+        
+            const icon1 = document.createElement('img');
+            icon1.classList.add('type-icon');
+            icon1.setAttribute('alt', 'TypeScriptIcon');
+            icon1.src = './assets/icons/typescript.png';
+        
+            const techName = document.createElement('p');
+            techName.textContent = `${skill.skillName}`;
+
+            card.addEventListener('click', () => {
+    
+                renderingDetailSkill(skill)
+                window.scrollTo({top:250})
+                cardContainer.classList.remove('inactive');
+                body.classList.add('no-scroll');
+
+            })
+        
+            card.append(icon1, techName)
+            skillTechContianer.append(card)
+        }
+
+    })
+}
+
+cardSkillRender(cards)
+
+function renderingDetailSkill(element){
+
+    const cardDetail = document.createElement('div');
+    cardDetail.classList.add('skill-detail');
+
+    if (element.categoryBlock != 'fifth-tech-block') {
+    
+        const icon1 = document.createElement('i');
+        icon1.classList.add('fa-brands', `${element.iconClass1}`, 'skills-icons');
+            
+        const techName = document.createElement('p');
+        techName.textContent = `${element.skillName}`;
+    
+        const icon2 = document.createElement('i');
+        icon2.classList.add('fa-brands', `${element.iconClass2}`, 'skills-icons');
+    
+        const cardDetailTextContainer = document.createElement('div');
+        cardDetailTextContainer.classList.add('card-detail-text-container');
+    
+        const techText = document.createElement('p');
+        techText.textContent = `${element.text}`;
+
+        const iconClose = document.createElement('i');
+        iconClose.classList.add('fa-solid', 'fa-xmark', 'skills-icons');
+        iconClose.addEventListener('click', () => {
+
+            cardContainer.classList.add('inactive');
+            body.classList.remove('no-scroll');
+            cardDetail.remove();
+
+        })
+
+        const link = document.createElement('a');
+        link.setAttribute('href', 'www.linkedin.com/in/fernando-yepes');
+        link.setAttribute('target', '_blank');
+        link.classList.add('linkedin-icon', 'deatilLinkIcon');
+
+        const linkedInIcon = document.createElement('i');
+        linkedInIcon.classList.add('fa-brands', 'fa-linkedin');
+        
+        link.append(linkedInIcon);
+        cardDetailTextContainer.append(techText);
+        cardDetail.append(icon1, techName, icon2, cardDetailTextContainer, iconClose, link);
+        cardContainer.append(cardDetail);
+        
+
+    }else{
+        
+        const icon1 = document.createElement('img');
+        icon1.classList.add('type-icon');
+        icon1.setAttribute('alt', 'TypeScriptIcon');
+        icon1.src = './assets/icons/typescript.png';
+            
+        const techName = document.createElement('p');
+        techName.textContent = `${element.skillName}`;
+    
+        const cardDetailTextContainer = document.createElement('div');
+        cardDetailTextContainer.classList.add('card-detail-text-container');
+    
+        const techText = document.createElement('p');
+        techText.textContent = `${element.text}`;
+
+        const iconClose = document.createElement('i');
+        iconClose.classList.add('fa-solid', 'fa-xmark', 'skills-icons');
+        iconClose.addEventListener('click', () => {
+
+            cardContainer.classList.add('inactive');
+            body.classList.remove('no-scroll');
+            cardDetail.remove();
+
+        })
+
+        cardDetailTextContainer.append(techText);
+        cardDetail.append(icon1, techName, cardDetailTextContainer, iconClose);
+        cardContainer.append(cardDetail);
+
+    }
+
+}
+
+
--- src/nodos.js
+++ src/nodos.js
@@ -5,11 +5,18 @@ const mobileMenu = document.querySelector('.mobile-menu');
 const contactForm = document.querySelector('.contact-form');
 const projectsSection = document.querySelector('#projects-section');
 const optionalProjects = document.querySelectorAll('.optional-projects');
-//Elements
+const skillsBlock = document.querySelectorAll('.skills-block');
+const cardContainer = document.querySelector('#card-container');
+const skillTechContianer = document.querySelector('#skills-tech-container');
 
+//Elements
 const stick1 = document.querySelector('.stick-1');
 const stick2 = document.querySelector('.stick-2');
 const stick3 = document.querySelector('.stick-3');
+
+const navbarLink = document.querySelectorAll('.navbar-link');
+
+const closeSkillDetail = document.querySelector('.fa-xmark');
 
 const blur = document.querySelector('.blur');
 const contactBlur = document.querySelector('.contact-blur');
